a221fd451115c24396c262ab3bf9ce9d0c29b4bd
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-11-13 13:19:08 +0700

    fix additional column

--- hooks/index-exam_schedules.php
+++ hooks/index-exam_schedules.php
@@ -14,13 +14,16 @@ if($filter)
     $where = (empty($where) ? 'WHERE ' : ' AND ') . $filter_query;
 }
 
+
+$additionalColumn = "";
 if($role->role_id == env('EXAM_MEMBER_ROLE_ID'))
 {
     // get user group : SELECT group_id FROM exam_group_member WHERE user_id = auth()->id
     $where = (empty($where) ? 'WHERE ' : ' AND ') . " status='PUBLISH' AND id IN (SELECT schedule_id FROM exam_schedule_groups WHERE group_id IN (SELECT group_id FROM exam_group_member WHERE user_id = ".auth()->id."))";
+    $additionalColumn = ", (SELECT COUNT(*) FROM exam_member_answers WHERE exam_member_answers.schedule_id = exam_schedules.id AND exam_member_answers.user_id = ".auth()->id.") as is_answered";
 }
 
-$db->query = "SELECT *, IF(randomize_question=1,'Ya','Tidak') randomize_question, IF(randomize_answer=1,'Ya','Tidak') randomize_answer, (SELECT COUNT(*) FROM exam_member_answers WHERE exam_member_answers.schedule_id = exam_schedules.id) as is_answered FROM $this->table $where ORDER BY ".$col_order." ".$order[0]['dir']." LIMIT $start,$length";
+$db->query = "SELECT *, IF(randomize_question=1,'Ya','Tidak') randomize_question, IF(randomize_answer=1,'Ya','Tidak') randomize_answer $additionalColumn FROM $this->table $where ORDER BY ".$col_order." ".$order[0]['dir']." LIMIT $start,$length";
 $data  = $this->db->exec('all');
 
 $total = $this->db->exists($this->table,$where,[
