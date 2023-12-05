98c2f0c86a64e6300db8a5751f5955932a14d19a
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-12-05 09:28:06 +0700

    fix update exam

--- hooks/additional-button-before-create-exam_group_member.php
+++ hooks/additional-button-before-create-exam_group_member.php
@@ -5,4 +5,4 @@ if(isset($_GET['filter']) && isset($_GET['filter']['group_id']))
 {
     $params['group_id'] = $_GET['filter']['group_id'];
 }
-return '<a href="'.routeTo('exam/groups/member/import',['group_id'=>$group_id]).'" class="btn btn-sm btn-primary"><i class="fas fa-upload"></i> '.__('exam.label.import').'</a> ';
+return '<a href="'.routeTo('exam/groups/member/import',$params).'" class="btn btn-sm btn-primary"><i class="fas fa-upload"></i> '.__('exam.label.import').'</a> ';
