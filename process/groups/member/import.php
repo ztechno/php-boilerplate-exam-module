5af0427894d2890966e6c622be2170b5ff15e49e
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-12-05 09:31:05 +0700

    import add select2

--- process/groups/member/import.php
+++ process/groups/member/import.php
@@ -94,4 +94,9 @@ Page::setBreadcrumbs([
     ]
 ]);
 
+Page::pushHead('<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />');
+Page::pushHead('<style>.select2,.select2-selection{height:38px!important;} .select2-container--default .select2-selection--single .select2-selection__rendered{line-height:38px!important;}.select2-selection__arrow{height:34px!important;}</style>');
+Page::pushFoot('<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>');
+Page::pushFoot("<script src='".asset('assets/crud/js/crud.js')."'></script>");
+
 return view('exam/views/groups/member/import', compact('group_id','groups'));
