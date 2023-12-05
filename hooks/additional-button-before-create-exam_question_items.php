74a1da525a8568a767b7eeaed4dfdfb5fec9837f
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-12-05 11:40:25 +0700

    fix params

--- hooks/additional-button-before-create-exam_question_items.php
+++ hooks/additional-button-before-create-exam_question_items.php
@@ -3,6 +3,6 @@
 $params = [];
 if(isset($_GET['filter']) && isset($_GET['filter']['question_id']))
 {
-    $prams['question_id'] = $_GET['filter']['question_id'];
+    $params['question_id'] = $_GET['filter']['question_id'];
 }
 return '<a href="'.routeTo('exam/questions/items/import',$params).'" class="btn btn-sm btn-primary"><i class="fas fa-upload"></i> '.__('exam.label.import').'</a> ';
