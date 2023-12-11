eeb4f6eb03a81b8b93bd6f528d0acbcd44b2f8ff
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-12-11 08:01:25 +0700

    presence list

--- views/groups/member/presence-list.php
+++ views/groups/member/presence-list.php
@@ -15,25 +15,49 @@
 </style>
 <div class="card-container">
     <div class="card-item">
-        <center>
-            <img src="<?=asset('assets/exam/top.jpg')?>" alt="" width="100%" height="180px">
-        </center>
         <div class="card-content">
-            <h4 align="center"><u>DAFTAR HADIR</u></h4>
+            <h2 align="center">
+                DAFTAR HADIR PESERTA<br>
+                PENILAIAN AKHIR SEMESTER<br>
+                TAHUN PELAJARAN 2023/2024
+            </h2>
+            <table cellpadding="5">
+                <tr>
+                    <td>Nama Sekolah</td>
+                    <td>:</td>
+                    <td>SMK Negeri 1 Pulau Rakyat</td>
+                </tr>
+                <tr>
+                    <td>Hari</td>
+                    <td>:</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Kelas</td>
+                    <td>:</td>
+                    <td><?=$member[0]->group_name?></td>
+                </tr>
+                <tr>
+                    <td>Mata Pelajaran</td>
+                    <td>:</td>
+                    <td></td>
+                </tr>
+            </table>
             <table cellpadding="5" cellspacing="0" width="100%" align="center" border="1" style="margin:0">
                 <tr>
                     <th width="20px">NO</th>
                     <th width="100px">NIS</th>
                     <th width="350px">NAMA</th>
-                    <th>KELAS</th>
-                    <th>TANDA TANGAN</th>
+                    <th colspan="2">TANDA TANGAN</th>
+                    <th>KETERANGAN</th>
                 </tr>
                 <?php foreach($member as $no => $_member): ?>
                 <tr>
                     <td><?=$no+1?></td>
                     <td><?=$_member->username?></td>
                     <td><?=$_member->name?></td>
-                    <td><?=$_member->group_name?></td>
+                    <td><?=$no%2==0?($no+1).'.':''?></td>
+                    <td><?=$no%2!=0?($no+1).'.':''?></td>
                     <td></td>
                 </tr>
                 <?php endforeach ?>
