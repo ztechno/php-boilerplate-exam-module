3fc45e57e87547b63e4de24ba799cb93312d1619
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-03-26 08:22:42 +0700

    generate by command

--- hooks/after-create-exam_schedules.php
+++ hooks/after-create-exam_schedules.php
@@ -1,63 +1,63 @@
 <?php
 
-foreach($_POST['group'] as $groupId)
-{
-    $db->insert('exam_schedule_groups', [
-        'schedule_id' => $data->id,
-        'group_id'    => $groupId
-    ]);
-}
+// foreach($_POST['group'] as $groupId)
+// {
+//     $db->insert('exam_schedule_groups', [
+//         'schedule_id' => $data->id,
+//         'group_id'    => $groupId
+//     ]);
+// }
 
-if(!empty($_POST['question']))
-{
-    $db->insert('exam_schedule_questions',[
-        'schedule_id' => $data->id,
-        'question_id' => $_POST['question']
-    ]);
-}
+// if(!empty($_POST['question']))
+// {
+//     $db->insert('exam_schedule_questions',[
+//         'schedule_id' => $data->id,
+//         'question_id' => $_POST['question']
+//     ]);
+// }
 
-// generate user data
-$schedule = $data;
-$schedule_id = $data->id;
-if(!empty($_POST['question']))
-{
-    $schedule->question = $db->single('exam_questions', "id = (SELECT question_id FROM exam_schedule_questions WHERE schedule_id = $schedule_id)");
+// // generate user data
+// $schedule = $data;
+// $schedule_id = $data->id;
+// if(!empty($_POST['question']))
+// {
+//     $schedule->question = $db->single('exam_questions', "id = (SELECT question_id FROM exam_schedule_questions WHERE schedule_id = $schedule_id)");
     
-    $db->query = "SELECT * FROM users WHERE id IN (SELECT user_id FROM exam_group_member WHERE group_id IN (SELECT group_id FROM exam_schedule_groups WHERE schedule_id = $schedule_id))";
-    $users = $db->exec('all');
-    foreach($users as $user)
-    {    
-        $db->query = "SELECT * FROM exam_question_items WHERE question_id = ".$schedule->question->id;
-        // random
-        if($schedule->randomize_question)
-        {
-            $db->query .= " ORDER BY RAND()";
-        }
+//     $db->query = "SELECT * FROM users WHERE id IN (SELECT user_id FROM exam_group_member WHERE group_id IN (SELECT group_id FROM exam_schedule_groups WHERE schedule_id = $schedule_id))";
+//     $users = $db->exec('all');
+//     foreach($users as $user)
+//     {    
+//         $db->query = "SELECT * FROM exam_question_items WHERE question_id = ".$schedule->question->id;
+//         // random
+//         if($schedule->randomize_question)
+//         {
+//             $db->query .= " ORDER BY RAND()";
+//         }
     
-        // jumlah soal
-        if($schedule->question_showed)
-        {
-            $db->query .= " LIMIT $schedule->question_showed";
-        }
-        $items = $db->exec('all');
+//         // jumlah soal
+//         if($schedule->question_showed)
+//         {
+//             $db->query .= " LIMIT $schedule->question_showed";
+//         }
+//         $items = $db->exec('all');
     
-        $randomize_answer = $schedule->randomize_answer;
-        $items = array_map(function($item) use ($db, $randomize_answer){
-            $db->query = "SELECT id, item_id, description FROM exam_question_answers WHERE item_id=$item->id";
-            if($randomize_answer)
-            {
-                $db->query .= " ORDER BY RAND()";
-            }
+//         $randomize_answer = $schedule->randomize_answer;
+//         $items = array_map(function($item) use ($db, $randomize_answer){
+//             $db->query = "SELECT id, item_id, description FROM exam_question_answers WHERE item_id=$item->id";
+//             if($randomize_answer)
+//             {
+//                 $db->query .= " ORDER BY RAND()";
+//             }
     
-            $item->answers = $db->exec('all');
+//             $item->answers = $db->exec('all');
     
-            return $item;
-        }, $items);
+//             return $item;
+//         }, $items);
     
-        $db->insert('exam_schedule_user_data', [
-            'schedule_id' => $schedule_id,
-            'user_id' => $user->id,
-            'data' => json_encode($items)
-        ]);
-    }
-}
+//         $db->insert('exam_schedule_user_data', [
+//             'schedule_id' => $schedule_id,
+//             'user_id' => $user->id,
+//             'data' => json_encode($items)
+//         ]);
+//     }
+// }
