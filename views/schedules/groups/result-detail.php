8caeb89ca59c7e7f9df71f6ec22a92adceb11ede
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-04-01 15:07:11 +0700

    decode

--- views/schedules/groups/result-detail.php
+++ views/schedules/groups/result-detail.php
@@ -75,7 +75,7 @@ input[type="radio"] {
             if(empty($data->answers))
             {
                 $correction = $normalizeAnswers[$data->id]->score == NULL ? $correction*0 : $correction*1;
-                echo isset($normalizeAnswers[$data->id]) ? "<b>JAWABAN : </b>".$normalizeAnswers[$data->id]->answer_id . '<br><br>' . ($normalizeAnswers[$data->id]->score == null ? '<input type="number" class="form-control" name="score['.$data->id.']" value="0" step=".1" min="0" max="1">' : ($normalizeAnswers[$data->id]->score != '0' ? '<span class="badge bg-success">Skor : '.$normalizeAnswers[$data->id]->score.'</span>' : '<span class="badge bg-danger">Skor : 0</span>') ) : '';
+                echo isset($normalizeAnswers[$data->id]) ? "<b>JAWABAN : </b>".htmlspecialchars_decode($normalizeAnswers[$data->id]->answer_id) . '<br><br>' . ($normalizeAnswers[$data->id]->score == null ? '<input type="number" class="form-control" name="score['.$data->id.']" value="0" step=".1" min="0" max="1">' : ($normalizeAnswers[$data->id]->score != '0' ? '<span class="badge bg-success">Skor : '.$normalizeAnswers[$data->id]->score.'</span>' : '<span class="badge bg-danger">Skor : 0</span>') ) : '';
             }
             else
             {
