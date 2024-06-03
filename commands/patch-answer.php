e5be2659d89676f49838ff086bb7c0e6bab41808
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-06-04 00:26:04 +0700

    add checker

--- commands/patch-answer.php
+++ commands/patch-answer.php
@@ -16,14 +16,23 @@ $parent_path = Utility::parentPath();
 $db = new Database;
 
 // get questions
-$questions = $db->all('exam_question_items', ['question_id' => $question_id]);
-foreach($questions as $index => $question)
+$question = $db->single('exam_question_items', ['id' => $question_id]);
+
+if(empty($question))
+{
+    echo "Question not found!";
+    die();
+}
+
+echo "Question found : ".$question->name."\n";
+$items = $db->all('exam_question_items', ['question_id' => $question->id]);
+foreach($items as $index => $item)
 {
     $no = $index + 1;
-    $answers = $db->all('exam_question_answers', ['item_id' => $question->id]);
+    $answers = $db->all('exam_question_answers', ['item_id' => $item->id]);
     foreach(range('a', 'e') as $alpha)
     {
-        $filename = 'q-'.$question->id.'/'.$no.$alpha.'.png';
+        $filename = 'q-'.$item->id.'/'.$no.$alpha.'.png';
         if(Storage::exists($filename))
         {
             echo $filename."\n";
