ab50374c2c3b9f6b30dfb54dff1efc03f1fd181a
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-12-06 09:23:21 +0700

    extract menu

--- config/menu.php
+++ config/menu.php
@@ -2,33 +2,37 @@
 
 return [
     [
-        'label' => 'exam.menu.exams',
-        'icon'  => 'fa-fw fa-xl me-2 fa-solid fa-graduation-cap',
+        'label' => 'exam.menu.groups',
+        'route' => routeTo('crud/index',['table'=>'exam_groups']),
+        'icon' => 'fa-fw fa-xl me-2 fa-solid fa-layer-group',
         'activeState' => [
             'exam.exam_groups',
             'exam.exam_group_member',
-            'exam.exam_questions',
-            'exam.exam_schedules',
-        ],
-        'items' => [
-            [
-                'label' => 'exam.menu.groups',
-                'route' => routeTo('crud/index',['table'=>'exam_groups']),
-                'activeState' => [
-                    'exam.exam_groups',
-                    'exam.exam_group_member',
-                ]
-            ],
-            [
-                'label' => 'exam.menu.questions',
-                'route' => routeTo('crud/index',['table'=>'exam_questions']),
-                'activeState' => 'exam.exam_questions'
-            ],
-            [
-                'label' => 'exam.menu.schedules',
-                'route' => routeTo('crud/index',['table'=>'exam_schedules']),
-                'activeState' => 'exam.exam_schedules'
-            ]
         ]
     ],
+    [
+        'label' => 'exam.menu.questions',
+        'route' => routeTo('crud/index',['table'=>'exam_questions']),
+        'icon' => 'fa-fw fa-xl me-2 fa-solid fa-file-circle-question',
+        'activeState' => 'exam.exam_questions'
+    ],
+    [
+        'label' => 'exam.menu.schedules',
+        'route' => routeTo('crud/index',['table'=>'exam_schedules']),
+        'icon' => 'fa-fw fa-xl me-2 fa-solid fa-calendar-days',
+        'activeState' => 'exam.exam_schedules'
+    ],
+    // [
+    //     'label' => 'exam.menu.exams',
+    //     'icon'  => 'fa-fw fa-xl me-2 fa-solid fa-graduation-cap',
+    //     'activeState' => [
+    //         'exam.exam_groups',
+    //         'exam.exam_group_member',
+    //         'exam.exam_questions',
+    //         'exam.exam_schedules',
+    //     ],
+    //     'items' => [
+            
+    //     ]
+    // ],
 ];
